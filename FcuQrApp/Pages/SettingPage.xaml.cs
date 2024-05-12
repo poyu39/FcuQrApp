80d30a2485fc28e027092dd3468308b8370a3e0a
poyu39 <bruce920309@gmail.com>
2024-05-12 21:15:22 +0800

    feat: 調整設定頁面

--- FcuQrApp/Pages/SettingPage.xaml.cs
+++ FcuQrApp/Pages/SettingPage.xaml.cs
@@ -10,8 +10,8 @@ public partial class SettingPage : ContentPage
 
 	protected override void OnAppearing()
 	{
-        NID.Text = Preferences.Default.Get("NID", "null");
-        Password.Text = Preferences.Default.Get("Password", "null");
+        NID.Text = Preferences.Default.Get("NID", "");
+        Password.Text = Preferences.Default.Get("Password", "");
     }
 
 	public void SaveButton_Clicked(object sender, EventArgs e)
